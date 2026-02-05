05c5d410c9fc07885e59db1ce2afee6c7075e1ed
Segni Mideksa <segnim21@gmail.com>
2026-02-05 22:23:47 +0300

    add file about JSON

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,4 +1,4 @@
-document.addEventListener
+ /*document.addEventListener
 ('DOMContentLoaded', function(){
 // Array of quote objects
 let quotes = [
@@ -74,13 +74,80 @@ function createAddQuoteForm()
     formDiv.appendChild(categoryInput);
     formDiv.appendChild(addButton);
 
-    document.body.appendChild(formDiv);
+    document.body.appendChild(formDiv);}
 
     //Event listener
     newQuoteBtn.addEventListener('click',showRandomQuote);
 
     //Create form on page load
     // Run once on load
-    createAddQuoteForm();}
+    createAddQuoteForm();
 
-showRandomQuote(); });
+showRandomQuote(); });*/
+
+document.addEventListener('DOMContentLoaded', function () {
+    
+    // Load quotes from localStorage or default
+    let quotes = JSON.parse(localStorage.getItem('quotes')) || [
+        { text:'Learning never exhausts the mind.', category: 'Education'},
+        { text: 'Success is not final, failure is not fatal.', category:'Motivation'}
+    ];
+
+    const quoteDisplay = document.getElementById('quoteDisplay');
+    const newQuoteBtn = document.getElementById('newQuote');
+    const exportBtn = document.getElementById('exportBtn');
+    const importFile = document.getElementById('importFile');
+
+    // Save quotes to localstorage
+    function saveQuotes() {
+        localStorage.setItem('quotes', JSON.stringify(quotes));
+    }
+    // show random quote
+
+    function showRandomQuote() {
+        const index = Math.floor(Math.random() * quotes.length);
+        sessionStorage.setItem('lastQuote', index);
+        quoteDisplay.innerHTML = '';
+
+        const p = document.createElement('p');
+        p.textContent = quotes[index].text;
+
+        const small = document.createElement('small');
+        small.textContent = 'Category: ' + quotes[index].category;
+
+        quoteDisplay.appendChild(p);
+        quoteDisplay.appendChild(small);
+    } 
+
+    // Export JSON
+    exportBtn.addEventListener('click', function () {
+        const data = JSON.stringify(quotes);
+        const blob = new Blob([data], { type: 'application/json'});
+        const url = URL.createObjectURL(blob);
+
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'quotes.json';
+        a.click();
+
+        URL.revokeObjectURL(url);
+    });
+
+    // Import JSON
+    importFile.addEventListener('change', function (event) {
+        
+        const reader = new FileReader();
+        reader.onload = function () {
+            const importQuotes = JSON.parse(reader.result);
+            quotes.push(...importQuotes);
+            saveQuotes();
+            alert ('Quotes imported successfully');
+        };
+        reader.readAsText(event.target.files[0]);
+    });
+
+    newQuoteBtn.addEventListener('click', showRandomQuote);
+
+    saveQuotes();
+    showRandomQuote();
+});
